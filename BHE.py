86a0aed492476c044b0a59d96268f95ec01c9d46
Ryder3x <136396464+Ryder3x@users.noreply.github.com>
2023-06-13 09:11:57 +0700

    Add files via upload

---
+++ BHE.py
@@ -0,0 +1,85 @@
+import lzma
+import base64 as b64
+import random as rd
+# ©$⁡⁮‮⁯‫⁯⁮‭⁠⁯‍‌‌‌‌‌⁯‍⁯‭‭‭​⁥‌‪⁯‌‪‪‌‪⁯͏‌‌‌‌‌⁠⁯⁥͏⁯‬​⁠͏⁯‌‌᠎⁭‌‪⁯⁮⁤᠋‌‌‎⁯​⁣‪‪‭⁯‭‫⁯‮⁥͏‬⁥⁫‭⁯͏⁯⁠‎⁯‪⁭‭⁯⁡‭‎⁯͏⁭⁣⁠⁯⁥⁠͏⁯⁥‎⁯‎⁯⁨‫⁯‪‍⁯‌‮⁯⁬͏⁯⁫⁫⁦᠋⁤͏⁯⁠⁩‌⁯‍⁥‪⁯‬‭⁦‪⁭⁫‭⁯‫⁥⁠͏‌⁯‪⁯⁫‭⁯᠎‭⁯‫​‫‍‮⁯⁭⁠⁯⁩⁨⁠‭⁦⁠⁠⁯‮⁡‮‪‌⁯⁣‍‌⁯⁠⁠᠎⁬‪⁦⁩‌‍⁯‬‎⁯‫‫⁯‮‍​‫⁯‍‫⁯‭͏⁯‪‪⁯‫⁯‫⁯᠋‌͏‭⁯‌⁯‌⁨͏⁥⁭⁠‪‍⁯⁡​‮⁯⁥‌͏⁯‪‎⁯‌‭‭⁦⁠⁯⁨⁠⁤͏‪⁮᠋‫‬‪‪‌‮⁯‪⁯‭‍‪⁯͏⁯⁭‌͏͏⁯⁠‮⁯‮‌⁩⁩᠎‮⁯⁩⁫͏‪‍⁯⁡⁡‎⁯⁠⁣⁠⁥⁠‮⁯‫‮⁯‭‌⁯⁮‪⁬‮⁯⁫⁤⁫‮⁯⁣‫‪‮͏⁯‎⁯͏⁬᠎‪⁯⁩‎⁯⁤͏͏⁯‮⁯‭⁯‎⁯⁥⁮‪‫‮͏⁯⁩‮⁥​͏͏⁯᠎‮⁣‍͏᠋᠎​‍⁯⁠⁯​‍⁯‮⁦᠋͏⁯⁭‎⁯᠋‎⁯⁥‭⁥⁩‎⁯‭⁯⁮‭⁯‮⁯‍⁠⁮⁮‫⁯‭͏⁯᠎‫‫‌⁯‭⁯⁨⁦⁮⁦⁨⁥‍‮⁯‫‫⁤⁩᠎⁠⁣‎⁯⁥‎⁯᠋‮⁯‍⁠⁦⁬⁦⁨‮͏⁯‍⁯⁬⁥​‪⁤⁦⁣͏⁯‍⁯᠎‭⁯‭​‫⁣‍⁯‭⁯⁦‪⁡‮⁯‌⁮‪‫⁭‪⁯᠎‮⁯‌⁫⁠⁯‪⁯‭⁯‮⁯͏⁯⁤⁡‌‫⁯‫⁯⁨⁮⁬‫‌‍⁯͏‫⁯‫⁯⁨‪⁯‮⁦‭͏‪⁯‌⁦‭⁩‎⁯⁭‌⁯‪‌⁯⁮᠎᠎⁮⁡⁩‫⁯͏⁯⁭⁩‌‫‮⁯‬⁭⁨⁮‭⁯⁨​⁡‮⁯᠋⁤⁦⁠⁯⁨⁦⁤‎⁯᠋⁠⁯‫⁣͏⁫‌⁯⁫͏‫⁯⁡⁠‍͏‮⁯⁡͏⁯͏⁦‬͏‫⁯⁭‬͏⁠᠎⁠‪⁤⁦‭⁯‮‍⁯᠎‍⁬‍⁦‫⁯⁭‭⁯‍⁡⁬͏‬‫⁯‌‌⁯‍⁯᠋⁮‍‮⁯⁤͏⁠⁯⁡‌⁯​⁠‪⁯‪⁬͏⁯‮⁯⁩⁣᠎⁭‭‍⁯⁬‪⁯‍⁦⁫⁭᠋͏⁩᠎⁮⁥᠎‎⁯‌⁯᠎​‪⁯‮⁯‭᠋‭⁯͏‎⁯⁮‫⁯‍‫⁤⁦⁮⁩⁨⁨‍͏⁯᠋‫᠋᠋᠋‬͏⁯⁡‪⁦⁨⁡‫⁯⁮‌⁥⁨‫‍͏͏‫‮⁯͏⁨‌⁯‪͏‍⁦⁠⁯⁬⁠⁯⁮‎⁯‭⁠⁯‫‬‍᠎᠎⁥‭⁯᠎​͏⁯‍⁯⁩‪⁮‮⁣‍͏⁯⁡‌⁯‪᠎⁦‮᠋⁥⁠⁯⁠⁯⁨᠋‮⁯‫⁨⁫⁭‍⁯‭‪⁯⁬‬‌⁯⁤⁫‮⁯͏⁯᠋‮‌⁯⁨‍⁯‫⁯‫⁯‪⁤⁮⁠⁯͏‪⁬‫⁣‪⁯‭⁯‍⁯⁨‍⁯⁣⁥⁫⁠‭⁯‍⁯͏⁮‫⁠‫⁯‫⁭‌‮‭⁯⁤⁭‬‭‫⁬‌⁫⁮‎⁯⁬᠎⁨͏⁯⁬​͏⁯⁩⁠⁯͏⁯‪⁤᠋‮‎⁯⁣‬᠋⁦‎⁯‎⁯‌͏‬᠋‭‭⁯‬⁭​‌⁬‪⁯᠋‮⁯⁡⁠‪‫⁯‍⁥⁨⁣⁣⁦⁥‌⁯⁨⁩͏‌‌᠎͏‍⁩⁭‪⁯⁡‬⁦⁠⁯‭⁯⁨‭‪‌‍⁠⁯‌‪⁩⁭⁥⁡‍‬⁦‫⁯‫⁯‭‫⁯᠎‮⁯⁨⁩‍‭⁥‌⁬͏⁡⁩⁩͏⁩‫⁥‮⁯⁨‮⁯⁬‎⁯‫⁯⁮‫⁮⁩⁠⁯‌‫⁠⁯‮⁯⁨⁠‫⁯⁡͏⁠‍⁯⁠⁯⁨‍⁯⁩͏​͏‌͏⁯⁠⁫⁦‎⁯‮⁯‍⁫⁭‫‌⁯⁤‭⁮⁡͏⁯᠎‪‭⁡⁦‬‫⁯‮‪⁯‍‌​⁡‫᠎⁨⁣‬‫⁦͏‍‎⁯͏᠎⁨‌⁯⁡͏‮⁯͏⁮⁮‎⁯⁨⁬⁭‌‫⁯⁫⁨‫⁯‍⁯‍​‭‮⁯‫‍⁯⁤⁦‫⁯⁥͏‬⁠⁯‮⁩‎⁯‫​⁦​⁫‍͏‭‍⁯‫‬͏⁫‎⁯‍⁯⁡͏⁯⁠⁯⁣‎⁯᠎‍⁯‭⁯‫⁨​‪⁤᠎⁦⁡⁠᠎‌⁯⁬‌⁯‌⁯‭⁯‮⁯‪⁯‫⁯‌⁠⁯‍⁯‬‫⁮‍⁯⁦⁤⁨⁫​‭‍⁯​‌⁠‪⁥⁭᠎⁤⁫⁡⁥‮‭‫᠎‮⁯‌‎⁯⁥‮⁯‬‬⁩͏⁤͏‫⁯⁦‍⁯‪⁯⁮⁡‌⁯‌⁯⁩⁨͏⁯⁡⁭⁫⁨⁡‭⁯⁬͏⁯‫⁯‮⁯᠎​⁩⁩​⁫⁠⁯‮⁯‮‌‌‌‌‌‌͏‪⁬‫͏‍⁯͏⁯⁮᠋‫⁦⁭⁨‌‌⁥‮⁯‮‪⁯⁫⁮⁥‌‌‌͏⁯‍⁯‭⁫​‫⁯⁠⁨͏‪⁯⁨⁡᠋‎⁯‪⁯‌‌‌‌‌‌‌‮‭⁤‍
+
+"""
+Copyright owned by Ryder3x
+
+----------------------------------------------------------------------------
+File: BHE.py
+Author: Ryder3x
+Created: 13th June 2023
+Copyright owned by Ryder3x
+
+Description: This is the source code of the BHE tool. All intellectual
+             property rights are owned by the author. Any unauthorized copying,
+             modification, or distribution without prior written permission from
+             the author is strictly prohibited.
+----------------------------------------------------------------------------
+"""
+
+
+def new_map(m, pwd):
+    rd.seed(pwd)
+    ks = list(m.keys())
+    rd.shuffle(ks)
+    m = {k: m[ks[i]] for i, k in enumerate(m)}
+    return m
+
+def enc(t):
+    t = lzma.compress(t.encode("utf-8"), preset=9)
+    b32 = b64.b32encode(t).decode("utf-8")
+    return b32
+
+def dec(t):
+    t = b64.b32decode(t.encode("utf-8"))
+    de = lzma.decompress(t).decode("utf-8")
+    return de
+
+
+def encode(t, m):
+    e = ""
+    for _ in enc(t):
+        if _ in m:
+            e += m[_]
+        else:
+            e += _
+    return f"${e}"
+
+def decode(t, m):
+    e, i, t = "", 0, t[1:]
+    while i < len(t):
+        _ = t[i:i+2] if t[i:i+2] in m.values() else t[i]
+        e += list(m.keys())[list(m.values()).index(_)]
+        i += 2 if _ == t[i:i+2] else 1
+    return dec(e)
+
+orig_map={"A":"\u034F","B":"\u200C","C":"\u200E","D":"\u200D","E":"\u202A","F":"\u202B","G":"\u202C","H":"\u202D","I":"\u202E","J":"\u2060","K":"\u2061","L":"\u2062","M":"\u2063","N":"\u2064","O":"\u2065","P":"\u2066","Q":"\u2067","R":"\u2068","S":"\u2069","T":"\u180E","U":"\u200B","V":"\u206A","W":"\u206B","X":"\u206C","Y":"\u206D","Z":"\u206E","0":"\u034F\u206F","1":"\u200C\u206F","2":"\u200E\u206F","3":"\u200D\u206F","4":"\u202A\u206F","5":"\u202B\u206F","6":"\u202C\u206F","7":"\u202D\u206F","8":"\u202E\u206F","9":"\u2060\u206F","=":"\u180B"}
+
+IPR = "\n\n\n# MÃ HÓA HỐ ĐEN (BHE) TẠO BỞI @Ryder3x (https://github.com/)"
+
+def Encode(key, text):
+	map = new_map(orig_map, key)
+	enc = encode(f"{text}{IPR}", map)
+	return enc
+
+def Decode(key, text):
+	map = new_map(orig_map, key) 
+	dec = decode(text, map)
+	return dec
+
+
+def fileEncode(key, file):
+	map = new_map(orig_map, key)
+	data = open(f"{file}","r").read()
+	open(f"BHE(enc)_{file}.txt","w").write(encode(f"{data}{IPR}", map))
+	print(f"save encode file at BHE(enc)_{file}.txt")
+
+def fileDecode(key, file):
+	map = new_map(orig_map, key)
+	data = open(f"{file}","r").read()
+	open(f"BHE(dec)_{file}.txt","w").write(decode(data, map))
+	print(f"save encode file at BHE(dec)_{file}.txt")
+
+
